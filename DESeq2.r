3097336a42e70964bf18ad350595ad475c60e421
Ali-Mamivand <54739077+Ali-Mamivand@users.noreply.github.com>
2022-04-04 14:56:55 +0430

    Update DESeq2.r

--- DESeq2.r
+++ DESeq2.r
@@ -1,30 +1,28 @@
 ##---- loading packages --------------
+# important Note: use the last version of TCGAbiolinks and TCGAbiolinksGUI.data (https://github.com/BioinformaticsFMRP/TCGAbiolinks)
 library(TCGAbiolinks)
 library(SummarizedExperiment)
 library(DESeq2)
 library(pheatmap)
 library(RColorBrewer)
 library(ggplot2)
-library(PoiClaClu)
 library(genefilter)
 library(AnnotationDbi)
+library(stringr)
 library(org.Hs.eg.db)
 library(ReportingTools)
 library(tibble)
 library(dplyr)
 library(tidyr)
 library(fgsea)
-
 ##----loading data---------------
 # Download GBM and LGG samples from TCGA
-set.seed(1000)
-lgg.samples <- matchedMetExp("TCGA-LGG", n = 20)
-gbm.samples <- matchedMetExp("TCGA-GBM", n = 20)
-samples <- c(lgg.samples,gbm.samples)
+sample_sheet <- read.delim("C:/path/to/working/directory/sample_sheet.tsv")
 query_exp <- GDCquery(project= c("TCGA-LGG","TCGA-GBM"), 
                       data.category = "Transcriptome Profiling", 
-                      workflow.type = 'HTSeq - Counts', 
-                      barcode = samples)
+                      workflow.type = 'STAR - Counts',
+                      data.type = 'Gene Expression Quantification',
+                      barcode = sample_sheet$Sample.ID)
 GDCdownload(query_exp)
 counts.table=GDCprepare(query_exp)
 Count.Table=assay(counts.table)
@@ -38,9 +36,10 @@ clinical_information = as.data.frame(colData(counts.table))
                                design = ~ project_id))
 ##------Quality Control-----------
 dim(ddsMat)
-#remove genes with 0 counts across all samples:
+#remove reads with 0 counts across all samples:
 dds <- ddsMat[ rowSums(counts(ddsMat)) > 1, ]
 dim(dds)
+# hierarchical clustering
 dds_vst <- vst(dds)
 head(assay(dds_vst), 3)
 
@@ -53,14 +52,7 @@ pheatmap(sampleDistMatrix,
          clustering_distance_rows=sampleDists,
          clustering_distance_cols=sampleDists,
          col=colors)
-poisd <- PoissonDistance(t(counts(dds)))
-samplePoisDistMatrix <- as.matrix( poisd$dd )
-rownames(samplePoisDistMatrix) <- dds_vst$project_id
-colnames(samplePoisDistMatrix) <- NULL
-pheatmap(samplePoisDistMatrix,
-         clustering_distance_rows=poisd$dd,
-         clustering_distance_cols=poisd$dd,
-         col=colors)
+
 #PCA 
 plotPCA(dds_vst, intgroup = c("project_id"))
 #Classical multidimensional scaling (MDS)
@@ -77,7 +69,15 @@ res.05 <- results(dds, alpha=.05)
 table(res.05$padj < .05)
 resLFC1 <- results(dds, lfcThreshold=1)
 table(resLFC1$padj < 0.05)
-# Visualization of gene expression
+##---Annotation-------------------
+columns(org.Hs.eg.db)
+rownames(res)= str_sub(rownames(res),1,15)
+res$symbol <- mapIds(org.Hs.eg.db,
+                     keys=row.names(res),
+                     column = "SYMBOL",
+                     keytype = "ENSEMBL",
+                     multiVals = "first")
+##----- Visualization of gene expression -----
 topGene <- rownames(resLFC1)[which.min(resLFC1$padj)]
 data <- plotCounts(dds, gene=topGene,
                    intgroup="project_id", returnData=TRUE)
@@ -102,13 +102,13 @@ mat <- assay(dds_vst)[ topVarGenes, ]
 mat <- mat - rowMeans(mat)
 df <- as.data.frame(colData(dds_vst)["project_id"])
 pheatmap(mat, annotation_col=df)
-##---Annotation-------------------
-columns(org.Hs.eg.db)
-res$symbol <- mapIds(org.Hs.eg.db,
-                     keys=row.names(res),
-                     column = "SYMBOL",
-                     keytype = "ENSEMBL",
-                     multiVals = "first")
+# volcano plot. The following function creates a PDF file (volcano.pdf) in the working directory:
+TCGAVisualize_volcano(x = res$log2FoldChange,y = res$padj,x.cut = c(-2,2),
+                      y.cut = 0.001,xlab = "Log2 (Fold Change)",height = 6,
+                      width = 8.5,names = res$symbol,highlight = 'OBI1-AS1',
+                      show.names='highlighted',
+                      label = c('Not Significant','Up-Regulated in LGG',
+                                'Down-Regulated in LGG'))
 ##---- report results--------
 res <- res[order(res$padj),]
 head(res)
@@ -125,7 +125,7 @@ browseURL(url)
 # Download mysigdb from https://www.gsea-msigdb.org/gsea/msigdb/ and put it 
 # in the same directory with this code.
 # Here I used h.all.v7.2.symbols.gmt:
-pathway_Dir = 'F:/RNA-Seq/glioma/LGG_Normal/' #set it to the your directory that contains gmt file
+pathway_Dir = 'F:/your/directory/' #set it to the your directory that contains gmt file
 pathway <- gmtPathways(paste0(pathway_Dir,"h.all.v7.2.symbols.gmt"))
 # show few lines from the pathways file
 head(pathway)
@@ -184,6 +184,5 @@ pdf('top_ten_patway.pdf',width = 8.5,height = 11)
 plotGseaTable(pathway[fgseaRes$pathway[1:10]], ranks, fgseaRes, 
               gseaParam=0.5)
 dev.off()
-
 
 
